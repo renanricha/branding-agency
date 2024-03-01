73d1a9f37a58daabde629510a7af1b58de839ad9
Renan Richa <renanricha@gmail.com>
2024-03-01 13:32:59 -0300

    Update index.jsx

--- projeto_agencia_design/src/components/Header/index.jsx
+++ projeto_agencia_design/src/components/Header/index.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from '../../assets/logo.png'
 import Moon from '../../assets/moon.png'
 import './index.css'
 
 export default function Header() {
+
+    const[cor, setCor] = useState(false)
+
+    const alteraCor = () => {
+        setCor(!cor)
+    }
+
     return(
         <div className="header-strip-dk">
             <div className='header-cont-dk'>
                 <img src={Logo} alt='Logo M' />
-                <button>
+                <button onClick={alteraCor}>
                     <img className="btnimg-dk" src={Moon} alt='Dark Mode' />
                 </button>
             </div>
