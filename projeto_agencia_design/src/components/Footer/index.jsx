b9b775d15f848b1fac0844e6c4c21c5d8c8baca0
Renan Richa <renanricha@gmail.com>
2024-03-02 20:43:48 -0300

    Fix for Footer

--- projeto_agencia_design/src/components/Footer/index.jsx
+++ projeto_agencia_design/src/components/Footer/index.jsx
@@ -8,7 +8,7 @@ import './index.css'
 
 export default function Footer(props) {
     return(
-        <div className={props.cor ? "footer-dk" : "footer"}>
+        <div className={props.cor ? "footer" : "footer-dk"}>
             <img className="img-logo" src={Logo} alt='Logo M' />
             <p>Ajudamos a criar uma personalidade digital construindo sua marca no ambiente online utilizando estratégias, ferramentas e tecnologias personalizadas.</p>
             <div className="social">
